9ce4781dc965b834d0b19e9c8bcbe5748bac43f5
Vazer Rahmani <vazer.dpr@gmail.com>
2019-05-05 05:19:05 +0100

    v1.0

--- qserve/core.js
+++ qserve/core.js
@@ -8,16 +8,24 @@ function Activate() {
     if (username.value != "" && password.value != "" && token.value != "") {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", 'https://us-central1-projectx-e9fc2.cloudfunctions.net/QServe', true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
         xhr.onreadystatechange = function() {
             if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
                 let ResponseObj = JSON.parse(xhr.responseText);
 
-                console.log(ResponseObj);
+                if (ResponseObj.LICENSE_KEY != null) {
+                    license_key.value = ResponseObj.LICENSE_KEY;
+                    license_key.style.display = 'flex';
+                } 
+
+                if (ResponseObj.ERROR != null) {
+                    alert("Invalid Authentication!");
+                }
             }
         }
 
-        xhr.send("username=" + username + "&password=" + password + "&token=" + token);
+        xhr.send("username=" + username.value + "&password=" + password.value+ "&token=" + token.value);
 
     } else {
         alert("Invalid Authentication!");
